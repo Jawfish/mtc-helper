41d7ed1e2d738ac91d684aea83250f38783f3963
James Fitzgerald <james@jawfish.dev>
2024-06-13 09:17:19 -0400

    finished implementing side-by-side view

--- src/store.ts
+++ src/store.ts
@@ -1,54 +1,62 @@
+import { createRoot, Root } from 'react-dom/client';
 import { createStore } from 'zustand/vanilla';
 
 export type Tab = 'edited' | 'original';
 
 export enum DiffViewState {
-  BLOCKS = 'blocks',
-  CLOSED = 'closed',
-  LINES = 'lines'
+    BLOCKS = 'blocks',
+    CLOSED = 'closed',
+    LINES = 'lines'
 }
 
 type StoreState = {
-  conversationOpen: boolean;
-  currentTab: Tab;
-  diffTogglesInserted: boolean;
-  diffView: DiffViewState;
-  editedContent: string;
-  editedTabHasListener: boolean;
-  metadataRemoved: boolean;
-  originalContent: string;
-  originalTabHasListener: boolean;
-  responseEditButtonHasListener: boolean;
-  saveButtonHasListener: boolean;
-  submitButtonHasListener: boolean;
-  orochiToolbarElement: HTMLDivElement | undefined;
-  // TODO: when "next" in the tests section is clicked, save them here to preserve
-  // the content so they can be copied even after moving to the QA scoring section
-  testContent: string;
+    conversationOpen: boolean;
+    currentTab: Tab;
+    diffModalOpen: boolean;
+    editedContent: string;
+    editedTabHasListener: boolean;
+    metadataRemoved: boolean;
+    originalContent: string;
+    originalTabHasListener: boolean;
+    responseEditButtonHasListener: boolean;
+    saveButtonHasListener: boolean;
+    submitButtonHasListener: boolean;
+    orochiToolbar: HTMLDivElement | undefined;
+    // TODO: when "next" in the tests section is clicked, save them here to preserve
+    // the content so they can be copied even after moving to the QA scoring section
+    reactRoot: Root | undefined;
+    testContent: string;
 };
 
 const initialState: StoreState = {
-  conversationOpen: false,
-  currentTab: 'edited',
-  diffTogglesInserted: false,
-  diffView: DiffViewState.CLOSED,
-  editedContent: '',
-  editedTabHasListener: false,
-  metadataRemoved: false,
-  originalContent: '',
-  originalTabHasListener: false,
-  responseEditButtonHasListener: false,
-  saveButtonHasListener: false,
-  submitButtonHasListener: false,
-  orochiToolbarElement: undefined,
-  testContent: ''
+    conversationOpen: false,
+    currentTab: 'edited',
+    diffModalOpen: false,
+    editedContent: '',
+    editedTabHasListener: false,
+    metadataRemoved: false,
+    originalContent: '',
+    originalTabHasListener: false,
+    orochiToolbar: undefined,
+    responseEditButtonHasListener: false,
+    saveButtonHasListener: false,
+    submitButtonHasListener: false,
+    reactRoot: (() => {
+        const rootElement = document.createElement('div');
+        rootElement.id = 'orochi-helper-root';
+        rootElement.style.zIndex = '999999';
+        document.body.appendChild(rootElement);
+        const root = createRoot(rootElement);
+        return root;
+    })(),
+    testContent: ''
 };
 
 export const store = createStore<StoreState>(() => ({
-  ...initialState
+    ...initialState
 }));
 
 export function resetStore() {
-  store.getState().orochiToolbarElement?.remove();
-  store.setState({ ...initialState });
+    store.getState().orochiToolbar?.remove();
+    store.setState({ ...initialState, reactRoot: store.getState().reactRoot });
 }
